5c943fce52d80db6a03d7ddf153696bfea610d41
hmrahim <h.m.rahimnet@gmail.com>
2025-11-11 07:30:17 +0300

    User model create done

---
+++ models/User.js
@@ -0,0 +1,26 @@
+const { model, Schema } = require("mongoose");
+const useerSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  rol: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+const User = model("User",useerSchema)
+module.exports = User
